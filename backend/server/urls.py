e6c0be0ad5761e668a77b860ca6340675d3802bc
riisatoro <riisa.toro@gmail.com>
2022-05-23 12:52:55 +0300

    Add swagger docs setup

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
+docs_urlpatterns = [
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', include(docs_urlpatterns))
 ]
