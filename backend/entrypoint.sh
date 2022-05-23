0c14907f90ec9586d8c2844496cdc1ffd966509a
riisatoro <riisa.toro@gmail.com>
2022-05-23 12:45:19 +0300

    Backend project setup

---
+++ backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo ""
+echo "Dependencies install"
+poetry install
+
+echo ""
+echo "Apply migrations"
+poetry run python manage.py migrate
+
+echo ""
+echo "Creating superuser"
+poetry run python manage.py createsuperuser --noinput
+
+echo ""
+echo "Server start up"
+poetry run python manage.py runserver  0.0.0.0:$PORT
