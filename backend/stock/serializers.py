6c4629e2adc20fbd24b42477a01f46f11747fa1c
riisatoro <riisa.toro@gmail.com>
2022-05-24 11:01:16 +0300

    Extend serializers for footballer

--- backend/stock/serializers.py
+++ backend/stock/serializers.py
@@ -5,14 +5,35 @@ from rest_framework.serializers import (
     Serializer,
 )
 
-from stock.models import Footballer, FootballerWeeksData
+from stock.models import (
+    Footballer, 
+    FootballerPosition, 
+    FootballerTeam,
+    FootballerWeeksData, 
+)
+
+
+class TeamSerializer(ModelSerializer):
+    
+    class Meta:
+        model = FootballerTeam
+        fields = ('name',)
+
+
+class PositionSerializer(ModelSerializer):
+    
+    class Meta:
+        model = FootballerPosition
+        fields = ('name',)
 
 
 class FootballerSerializer(ModelSerializer):
+    team = TeamSerializer()
+    position = PositionSerializer()
 
     class Meta:
         model = Footballer
-        fields = ('name',)
+        fields = ('name', 'team', 'position', 'price_dynamic',)
 
 
 class MarketFootballerSerializer(ModelSerializer):
