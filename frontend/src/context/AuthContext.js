f1f8f6c710cee2c2ea77537bb7049dacb4391d9f
riisatoro <riisa.toro@gmail.com>
2022-05-24 14:42:03 +0300

    Add proxy setup, context and auth urls

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,80 @@
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { 
+  ACCESS_TOKEN_URL, 
+  REFRESH_TOKEN_URL,
+  USER_INFO_URL,
+} from "../constants";
+
+export const AuthContext = React.createContext();
+
+const TOKEN_NAME = 'refresh-auth-token';
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState(window.localStorage.getItem(TOKEN_NAME))
+  const [isAuthenticated, setIsAuthenticated] = useState(Boolean(accessToken));
+
+  const apiInstance = axios.create({
+    baseURL: 'http://localhost:3000',
+    proxy: 'http://heroex-web/api',
+    headers: {
+      'Authorization':  `Bearer ${accessToken}`,
+      'Content-Type': 'multipart/form-data'
+    },
+  })
+
+  const saveRefresh = (token) => window.localStorage.setItem(TOKEN_NAME, token);
+
+  const updateTokens = ({data: {access, refresh}}) => {
+    setAccessToken(access);
+    setRefreshToken(refresh);
+    saveRefresh(refresh);
+  }
+
+  const login = (data) => axios.post(ACCESS_TOKEN_URL(), data).then(updateTokens);
+
+  const logout = () => {
+    window.localStorage.setItem(TOKEN_NAME, null);
+    setAccessToken(null);
+    setRefreshToken(null);
+    setIsAuthenticated(false);
+  }
+
+  const getNewAccessToken = () => {
+    axios.post(REFRESH_TOKEN_URL(), {refresh: refreshToken})
+    .then(({data: {access}}) => {
+        setAccessToken(access);
+        setIsAuthenticated(true);
+    })
+  }
+
+  const getUserData = () => {
+    apiInstance.get(USER_INFO_URL())
+    .then(({data}) =>setUser(data));
+  }
+
+  useEffect(() => {
+    if (accessToken) getUserData();
+    if (!accessToken && refreshToken) getNewAccessToken();
+    if (!refreshToken && !refreshToken) setIsAuthenticated(false);
+  }, [accessToken, refreshToken, isAuthenticated])
+  
+  const values = {
+      accessToken,
+      refreshToken,
+      updateTokens,
+      apiInstance,
+      saveRefresh,
+      isAuthenticated,
+      login,
+      logout,
+      user,
+  }
+
+  return (
+    <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+  );
+};
