47fb2d81c34e1d119c9248ee7d1be45590b47a09
riisatoro <riisa.toro@gmail.com>
2022-05-28 12:01:13 +0300

    Deploy update

--- backend/entrypoint-prod.sh
+++ backend/entrypoint-prod.sh
@@ -12,4 +12,4 @@ echo ""
 echo "Creating superuser"
 poetry run python manage.py createsuperuser --noinput
 
-poetry run python backend/manage.py runserver 0.0.0.0:$PORT
+poetry run python manage.py runserver 0.0.0.0:$PORT
