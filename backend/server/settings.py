3ed284b5e836db4625a425351160e2a9046a029c
riisatoro <riisa.toro@gmail.com>
2022-05-29 20:18:23 +0300

    Added trusted origin setting

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -142,3 +142,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+CSRF_TRUSTED_ORIGINS = getenv('CSRF_TRUSTED_ORIGINS', '').split(' ')
