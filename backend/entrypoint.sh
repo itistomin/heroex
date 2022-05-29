8254d5f349a2f561d8496ebb44859e2a7be11af4
riisatoro <riisa.toro@gmail.com>
2022-05-29 20:33:32 +0300

    Add dump data ability

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -13,5 +13,9 @@ echo "Creating superuser"
 poetry run python manage.py createsuperuser --noinput
 
 echo ""
+echo "Uploading dump data"
+poetry run python manage.py loaddata dumps/dump_26_05_2022.json
+
+echo ""
 echo "Server start up"
 poetry run python manage.py runserver  0.0.0.0:$PORT
