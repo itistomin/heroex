b3d143799dbccbb3ed90f768c478317b16432cc9
riisatoro <riisa.toro@gmail.com>
2022-05-31 21:04:08 +0300

    Add user creation on token recieving

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+from authentication.views import GetTokenCreateUserView
 
 urlpatterns = [
-    path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', GetTokenCreateUserView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from rest_framework import exceptions
+from rest_framework_simplejwt.settings import api_settings
+
+from authentication.models import User
+from stock.models import GameWeek
+
+
+class GetTokenCreateUserView(TokenObtainPairView):
+    
+    def post(self, request, *args, **kwargs):
+        print('POST HANDLED')
+        try:
+            User.objects.create_user(
+                **{
+                    'email': request.data.get('email'),
+                    'password': request.data.get('password'),
+                    'username': 'Random User',
+                    'week': GameWeek.objects.get(number=0),
+                    'is_active': True,
+                }
+            )
+        except Exception as E:
+            ...
+
+        serializer = TokenObtainPairSerializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            print('error (((')
+            raise InvalidToken(e.args[0])
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
